9eff99ee9fe7a72a34b2cf78c22aece6fef402c2
rethi <rethi@MyPC26>
2025-07-24 20:53:03 +0530

    This is inheritance programming

---
+++ C2TC_CoreJavaPrograms/src/DayFive/singleinheritance/SLInheritanceDemo.java
@@ -0,0 +1,11 @@
+package DayFive.singleinheritance;
+
+public class SLInheritanceDemo {
+
+	public static void main(String[] args) {
+		Student st = new Student("Rethika", "44563345", "Pune", 894309234, 26, "Pune university");
+		System.out.println(st);
+		
+	}
+
+}
