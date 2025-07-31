08370cf4ed962d8d257d8bf1811a851fd90acf54
rethi <rethi@MyPC26.mshome.net>
2025-07-31 09:17:58 +0530

    This is core java programming.

---
+++ C2TC_CoreJavaPrograms/src/DayNine/ObjectClassDemo.java
@@ -0,0 +1,36 @@
+package DayNine;
+
+class Sample{
+	
+}
+
+class Demo{
+	public Demo() {
+		System.out.println("In constructor");
+	}
+	
+	@Override
+	protected void finalize() throws Throwable{
+		System.out.println("In finalize");
+	}
+}
+
+public class ObjectClassDemo {
+	public static void main(String[] args) {
+		Sample s = new Sample();
+		Sample s1 = new Sample();
+		System.out.println(s.getClass().getName());
+		System.out.println(s.hashCode());
+		System.out.println(s1.hashCode());
+		
+		Demo d = new Demo();
+		System.out.println(d.hashCode());
+		
+		Demo d1 = new Demo();
+		System.out.println(d1.hashCode());
+		
+		d = null;
+		System.gc();
+		System.out.println(d1);
+	}
+}
