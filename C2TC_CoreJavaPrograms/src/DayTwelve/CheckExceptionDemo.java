483b0f8ea864b2bc91e60b8f6af51b59124855fc
rethi <rethi@MyPC26>
2025-08-01 22:56:29 +0530

    This is core java programming.

---
+++ C2TC_CoreJavaPrograms/src/DayTwelve/CheckExceptionDemo.java
@@ -0,0 +1,17 @@
+package DayTwelve;
+
+import java.io.IOException;
+
+public class CheckExceptionDemo {
+
+	public static void main(String[] args) {
+		try {
+			String str = CheckedException.acceptText();// reads line from console
+			System.out.println("Your string is " + str);
+		} catch (IOException e) {
+			System.err.println("An error occurred while reading input: " + e.getMessage());
+		}
+
+	}
+
+}
---
+++ C2TC_CoreJavaPrograms/src/DayTwelve/userdefinedexp/InvalidMarksException.java
@@ -0,0 +1,12 @@
+package DayTwelve.userdefinedexp;
+
+public class InvalidMarksException extends Exception{
+
+	public InvalidMarksException() {
+		super();
+	}
+
+	public InvalidMarksException(String message) {
+		super(message);
+	}
+}
